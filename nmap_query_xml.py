733fa0691b249f9f7d1b09555c50c6449c6627e6
Veids <veidsme@gmail.com>
2025-06-27 11:39:16 +0300

    refactor

--- nmap_query_xml.py
+++ nmap_query_xml.py
@@ -1,38 +1,119 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# This software must not be used by military or secret service organisations.
-# License: TODO
-# TODO: PEP 8
-
 import sys, argparse
-from libnmap.parser import NmapParser
+
+from libnmap.parser import NmapParser, NmapReport
+from string import Template
+
+
+class NmapQuery:
+    conf: int
+    service: str
+    pattern: str
+    state: str
+
+    def __init__(
+        self, pattern: str, conf: int = -1, service: str = "", state: str = "open"
+    ):
+        self.pattern = pattern
+        self.conf = conf
+        self.service = service
+        self.state = state
+
+    def _get_hostname(self, host):
+        return host.address if not host.hostnames else host.hostnames[0]
+
+    def _get_hostnames(self, host):
+        return host.address if not host.hostnames else ", ".join(set(host.hostnames))
+
+    def _parse_entity(self, host, service):
+        tpl = Template(self.pattern)
+
+        return tpl.safe_substitute(
+            hostname=self._get_hostname(host),
+            hostnames=host.address
+            if not host.hostnames
+            else ", ".join(set(host.hostnames)),
+            ip=host.address,
+            service=service.service,
+            s="s" if service.tunnel == "ssl" else "",
+            protocol=service.protocol,
+            port=service.port,
+            state=service.state,
+        )
+
+    def _service_matches(self, service) -> bool:
+        if not self.service:
+            return True
+        allowed = set(self.service.split(","))
+        return service.service in allowed
+
+    def _get_confidence(self, service) -> int:
+        try:
+            return int(getattr(service, "_service", {}).get("conf", 0))
+        except Exception:
+            return 0
+
+    def process(self, report: NmapReport):
+        for host in report.hosts:
+            for service in host.services:
+                if (
+                    (service.state == self.state or self.state == "all")
+                    and self._service_matches(service)
+                    and (self.conf == -1 or self._get_confidence(service) <= self.conf)
+                ):
+                    yield self._parse_entity(host, service)
+
 
 def parse_args():
-    greeter = '''
+    greeter = """
      ▐ ▄ • ▌ ▄ ·.  ▄▄▄·  ▄▄▄·    .▄▄▄  ▄• ▄▌▄▄▄ .▄▄▄   ▄· ▄▌    ▐▄• ▄ • ▌ ▄ ·. ▄▄▌
     •█▌▐█·██ ▐███▪▐█ ▀█ ▐█ ▄█    ▐▀•▀█ █▪██▌▀▄.▀·▀▄ █·▐█▪██▌     █▌█▌▪·██ ▐███▪██•
     ▐█▐▐▌▐█ ▌▐▌▐█·▄█▀▀█  ██▀·    █▌·.█▌█▌▐█▌▐▀▀▪▄▐▀▀▄ ▐█▌▐█▪     ·██· ▐█ ▌▐▌▐█·██▪
     ██▐█▌██ ██▌▐█▌▐█ ▪▐▌▐█▪·•    ▐█▪▄█·▐█▄█▌▐█▄▄▌▐█•█▌ ▐█▀·.    ▪▐█·█▌██ ██▌▐█▌▐█▌▐▌
-    ▀▀ █▪▀▀  █▪▀▀▀ ▀  ▀ .▀       ·▀▀█.  ▀▀▀  ▀▀▀ .▀  ▀  ▀ •     •▀▀ ▀▀▀▀  █▪▀▀▀.▀▀▀ '''
+    ▀▀ █▪▀▀  █▪▀▀▀ ▀  ▀ .▀       ·▀▀█.  ▀▀▀  ▀▀▀ .▀  ▀  ▀ •     •▀▀ ▀▀▀▀  █▪▀▀▀.▀▀▀ """
     for c in "█▐▌▄▀":
         greeter = greeter.replace(c, "\u001b[32m" + c + "\u001b[0m")
     version = "0.0.3#beta"
     url = "https://github.com/honze-net/nmap-query-xml"
 
-    parser = argparse.ArgumentParser(description="", epilog="Full documentation: %s\nThis software must not be used by military or secret service organisations." % url, formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser = argparse.ArgumentParser(
+        description="",
+        epilog="Full documentation: %s\nThis software must not be used by military or secret service organisations."
+        % url,
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+    )
     parser.add_argument("xml", help="path to Nmap XML file")
-    parser.add_argument("--service", help="Nmap service name to filter for. Default: Empty", default="", dest="service")
-    parser.add_argument("--pattern", help="Pattern for output. Default: %(default)s", default="{service}{s}://{hostname}:{port}", dest="pattern")
-    parser.add_argument("--state", help="Select a port state. Use \"all\" for all. Default: %(default)s", default="open", dest="state")
-    parser.add_argument("--conf", help="Select conf level", default=-1, dest="conf")
+    parser.add_argument(
+        "--service",
+        help="Nmap service name to filter for. Default: Empty",
+        default="",
+        dest="service",
+    )
+    parser.add_argument(
+        "--pattern",
+        help="Pattern for output. Default: %(default)s",
+        default="$service$s://$hostname:$port",
+        dest="pattern",
+    )
+    parser.add_argument(
+        "--state",
+        help='Select a port state. Use "all" for all. Default: %(default)s',
+        default="open",
+        dest="state",
+    )
+    parser.add_argument(
+        "--conf", help="Select conf level", type=int, default=-1, dest="conf"
+    )
 
-    if len(sys.argv) == 1: # If no arguments are specified, print greeter, help and exit.
+    if (
+        len(sys.argv) == 1
+    ):  # If no arguments are specified, print greeter, help and exit.
         print(greeter)
         print(("version %s %s\n" % (version, url)).center(80))
         parser.print_help()
         sys.exit(0)
 
     return parser.parse_args()
+
 
 def main():
     args = parse_args()
@@ -42,20 +123,13 @@ def main():
         print("Error: File %s not found." % args.xml)
         sys.exit(1)
 
-    for host in report.hosts:
-        for service in host.services:
-            if (service.state == args.state or args.state == "all") and (args.service == "" or service.service in args.service.split(",")) and (args.conf == -1 or service._service["conf"] == args.conf): # TODO: Test if this is precise enough
-                line = args.pattern
-                line = line.replace("{xmlfile}", args.xml)
-                line = line.replace("{hostname}", host.address if not host.hostnames else host.hostnames[0]) # TODO: Fix naive code.
-                line = line.replace("{hostnames}", host.address if not host.hostnames else ", ".join(list(set(host.hostnames)))) # TODO: Fix naive code.
-                line = line.replace("{ip}", host.address)
-                line = line.replace("{service}", service.service)
-                line = line.replace("{s}", "s" if service.tunnel == "ssl" else "")
-                line = line.replace("{protocol}", service.protocol)
-                line = line.replace("{port}", str(service.port))
-                line = line.replace("{state}", str(service.state))
-                print("%s" % line)
+    nq = NmapQuery(
+        pattern=args.pattern, conf=args.conf, service=args.service, state=args.state
+    )
 
-if __name__ == '__main__':
+    for line in nq.process(report):
+        print(line)
+
+
+if __name__ == "__main__":
     main()
